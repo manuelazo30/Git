f0b5f2e21f63535d2de4ecd8917855eb4f16f97d
manuelazo30 <manu@manuelazo.com>
2015-08-05 18:06:47 +0200

    Primera Práctica

---
+++ Node.js/servidor.js
@@ -0,0 +1,12 @@
+var http = require('http');
+http.createServer(
+
+	function(req, res)
+
+		{
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('Curso de Node.js\n');
+		}
+	).listen(3000, "127.0.0.1");
+
+console.log('Servidor arrancado en localhost en el puerto 3000');
